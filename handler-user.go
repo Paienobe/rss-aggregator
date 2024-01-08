1e7bb7cf16863e858ad174f8aedf48aeb4a6b16e
Progress Aienobe <paienobe@gmail.com>
2024-01-08 11:32:17 +0100

    fetching-users

--- handler-user.go
+++ handler-user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Paienobe/rssagg/internal/auth"
 	"github.com/Paienobe/rssagg/internal/database"
 	"github.com/google/uuid"
 )
@@ -30,6 +31,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintln("Couldn't create user:", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 		return
 	}
 
